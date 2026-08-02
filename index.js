82e74e6daffbeafbbd7d044d3847d8cacdd9511a
kohinoorsingh0 <kohinoorsingh040@gmail.com>
2026-08-02 07:06:11 +0530

    feat: add js logic in upper tools in toolbar

--- index.js
+++ index.js
@@ -14,3 +14,26 @@ const handleAddLink = () => {
     const url = prompt("Enter The URL")
     formatDoc("createLink", url)
 }
+
+
+let content = document.getElementsByClassName("editable-container")
+
+content[0].addEventListener("mouseenter", () => {
+    let anchors = content[0].querySelectorAll("a")
+
+    anchors.forEach((anchor) => {
+
+        anchor.target = "_blank";
+
+        anchor.addEventListener("mouseenter", (e) => {
+            content[0].setAttribute("contentEditable", "false")
+        })
+
+        anchor.addEventListener("mouseleave", (e) => {
+            content[0].setAttribute("contentEditable", "true")
+    })
+
+    })
+
+    
+})
